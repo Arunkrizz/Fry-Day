df28bab817cfc5d43c0eb11d53884478ee149392
Arun_krizz <arunkrizz1331@gmail.com>
2024-01-10 18:06:52 +0530

    dist20

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -32,8 +32,8 @@ export const ADMIN_RELIST_CATEGORY_URL = '/api/admin/reListCategory'
 export const ADMIN_BLOCK_UNBLOCK_USER = '/api/admin/block_UnblockUser'
 
 export const PROFILE_IMAGE_DIR_PATH = 'https://localhost:5000/UserProfileImages/'
-export const HOTEL_IMAGE_DIR_PATH = 'https://localhost:5000/restaurantImages/'
-export const POST_IMAGE_DIR_PATH = 'https://localhost:5000/PostImages/'
+export const HOTEL_IMAGE_DIR_PATH = 'https://43.205.83.14:5000/restaurantImages/'
+export const POST_IMAGE_DIR_PATH = 'https://sevenlier.online:5000/PostImages/'
 export const PRODUCT_IMAGE_DIR_PATH = 'https://sevenlier.online/ProductImages/'
 
 
