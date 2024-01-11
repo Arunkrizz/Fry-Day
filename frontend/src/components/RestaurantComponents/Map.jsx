89d4d3cbe7187cdcfb08bcc6bd722df23028c828
Arun_krizz <arunkrizz1331@gmail.com>
2024-01-11 10:17:03 +0530

    debugging map2

--- frontend/src/components/RestaurantComponents/Map.jsx
+++ frontend/src/components/RestaurantComponents/Map.jsx
@@ -12,7 +12,7 @@ const Map = ( props ) => {
 
     console.log(apikey,userPosition, restaurantPosition ," map order api")
 
-     
+     let router 
    
     useEffect(
     () => {
@@ -56,7 +56,7 @@ const Map = ( props ) => {
         }
     
         // Get an instance of the H.service.RoutingService8 service
-        const router = platform?.getRoutingService(null, 8);
+         router = platform?.getRoutingService(null, 8);
     
         // Define the routing service parameters
         const routingParams = {
