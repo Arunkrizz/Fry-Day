6026f743f9901ccbbe7fbde8a1b9b90e24ad2c9f
Arun_krizz <arunkrizz1331@gmail.com>
2024-03-18 19:57:34 +0530

    signup patch2.8

--- frontend/src/components/UserComponents/LoginuserModal.jsx
+++ frontend/src/components/UserComponents/LoginuserModal.jsx
@@ -151,7 +151,7 @@ const LoginuserModal = ({showLoginUserModal, setShowLoginUserModal,showRegisterU
           </Modal.Body>
           <Modal.Footer>
             <Button variant="primary"  onClick={async(e)=>{
-             await registerSubmit(e)
+              registerSubmit(e)
                handleSignUp()
               
              
